ba915094a8d16bc82c720d7619486fcaebfe3ee2
Iryna <irasokolova25@ukr.net>
2024-12-05 19:13:06 +0200

    в

---
+++ index.js
@@ -0,0 +1,29 @@
+const userName = 'Hello, Iryna!';
+const number = 10;
+
+userName.toLocaleUpperCase();
+
+const result = userName.includes('hello');
+
+if (result) {
+    const message = 'userName includes Hello'
+    console.log(message);
+}
+
+
+
+
+// fetch('https://newsapi.org/v2/everything?q=flowers&from=2024-11-04&sortBy=publishedAt&apiKey=3ce63eea477043d7a470d2b21dc5ab4b')
+
+
+// function name(arr) {
+//     arr.map()
+// }
+// name([1, 3, 4])
+// name([5, 2, 4])
+// name([{}, {},{}])
+
+// const b = function (param1) {
+    
+// }
+
