e7934ef9393c3f438d00375aa6a24bc1de2ab15a
Iryna <irasokolova25@ukr.net>
2024-12-30 16:57:44 +0200

    d

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const userName = 'Hello, Iryna!';
-const number = 10;
+// const userName = 'Hello, Iryna!';
+// const number = 10;
 
-userName.toLocaleUpperCase();
+// userName.toLocaleUpperCase();
 
-const result = userName.includes('hello');
+// const result = userName.includes('hello');
 
-if (result) {
-    const message = 'userName includes Hello'
-    console.log(message);
-}
+// if (result) {
+//     const message = 'userName includes Hello'
+//     console.log(message);
+// }
 
 
 
@@ -27,3 +27,46 @@ if (result) {
     
 // }
 
+
+
+// const result = fetch('https://newsapi.org/v2/everything?q=tesla&from=2024-11-14&sortBy=publishedAt&apiKey=3ce63eea477043d7a470d2b21dc5ab4b')
+//     .then((response) => {
+//         console.log(response);
+//        return response.json()
+//     })
+//     .then((jhjk) => {
+//         console.log(jhjk);
+//        return jhjk.articles;
+//     })
+//     .then((articles) => {
+//         console.log(articles);
+       
+//         articles.map((article) => {
+//             console.log(article);
+//         })
+//     })
+
+
+
+
+// console.log(result);
+
+const list = document.querySelector('ul')
+
+const numbers = [1, 2, 3, 4, 5]
+
+const markup = numbers.map((number) => {
+      return `<li>
+            <p>${number}</p>
+        </li>`
+}).join('')
+    
+    console.log(markup);
+   list.insertAdjacentHTML('beforeend', markup) 
+    
+    
+// [`<li><p>${number}</p></li>`, `<li>
+//             <p>${number}</p>
+//         </li>`, `<li>
+//             <p>${number}</p>
+//         </li>`]
