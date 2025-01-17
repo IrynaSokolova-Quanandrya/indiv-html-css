23684bcaeec8af04cf0054132531b11a74204f31
Iryna <irasokolova25@ukr.net>
2025-01-17 15:59:27 +0200

    finished

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-const delay = (time) => {
-    return new Promise((resolve) => {
-    setTimeout(()=>{resolve('hello')}, time)
-})
+const API_KEY = '24253422-4477825d93e6eb518eebc16ed'; // Замініть на ваш API-ключ
+const API_URL = 'https://pixabay.com/api/';
+const gallery = document.getElementById('image-gallery');
+const loadMoreBtn = document.getElementById('load-more-btn');
+
+let currentPage = 1; // Для пагінації
+
+async function fetchImages(page = 1) {
+  try {
+    const response = await fetch(
+      `${API_URL}?key=${API_KEY}&editors_choice=true&image_type=photo&per_page=12&page=${page}`
+    );
+    const data = await response.json();
+    return data.hits;
+  } catch (error) {
+    console.error('Помилка при завантаженні зображень:', error);
+    return [];
+  }
 }
 
-delay(1000)   	// wait 1000 ms / 1 sec
-.then(
-	// код который выполнится с задержкой
-  console.log("will be called in 1 sec")
-)
+function renderImages(images) {
+  images.forEach((image) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = image.webformatURL;
+    imgElement.alt = image.tags;
+    gallery.appendChild(imgElement);
+  });
+}
 
-delay(5000).then(alert("Hello world"))
+async function loadMoreImages() {
+  const images = await fetchImages(currentPage);
+  renderImages(images);
+  currentPage += 1;
+}
+
+// Початкове завантаження зображень
+loadMoreImages();
+
+// Обробник кліку на кнопку "Завантажити ще"
+loadMoreBtn.addEventListener('click', loadMoreImages);
